ffdb1555e20270dbef8a1a2a4c2e3fe58df0bbbe
Nicolas Hoffmann <nicolas.hoffmann@telecom-sudparis.eu>
2021-04-20 17:38:03 +0200

    Fix bugs

--- constructionSite.py
+++ constructionSite.py
@@ -1,6 +1,7 @@
 
 
 import numpy as np
+import random
 import gym
 
 
@@ -8,64 +9,66 @@ class ConstructionSite(gym.Env) :
 
     """ Custom "Construction Site" environment that follows gym interface """
 
-    GoUp = 0
-    GoLeft = 1
-    GoDown = 2
-    GoRight = 3
-    Pick = 4
-    Drop = 5
-
     def __init__(self, gridWidth=10, gridHeight=10) :
         super(ConstructionSite, self).__init__()
 
             # Properties of the construction site
         self.width = gridWidth
         self.height = gridHeight
-        self.map = np.zeroes((self.width, self.height))
+        self.map = np.zeros((self.width, self.height))
 
             # Properties of the operating machine
-        self.w = randint(self.width)
-        self.h = randint(self.height)
+        self.w = random.randint(0, self.width-1)
+        self.h = random.randint(0, self.height-1)
         self.isLoaded = False
 
             # Action space
-        n_actions = 6 # GoUp, GoLeft, GoRight, GoDown, Pick, Drop
-        self.action_space = gym.spaces.Discrete(n_actions)
+        self.GoUp = 0
+        self.GoLeft = 1
+        self.GoDown = 2
+        self.GoRight = 3
+        self.Pick = 4
+        self.Drop = 5
+        self.n_actions = 6 # GoUp, GoLeft, GoRight, GoDown, Pick, Drop
+        self.action_space = gym.spaces.Discrete(self.n_actions)
 
             # Observation space
         self.observation_space = gym.spaces.Box(
-            low=np.array([0, 0, 0]),
-            high=np.array([self.width, self.height, 2]),
-            dtype=np.int32
+            low=-10,
+            high=10,
+            shape=(self.width, self.height, 2)
         )
 
 
     def _computeObservation(self) :
-        positionMap = np.zeroes((self.width, self.height))
-        positionMap[self.w, self.h] = 1
-        obs = np.stack(self.map, positionMap)
+        positionMap = np.zeros((self.width, self.height, 1))
+        positionMap[self.w, self.h, 0] = 1
+        _map = np.expand_dims(self.map, axis=2)
+        obs = np.concatenate((_map, positionMap), axis=-1)
         return obs
 
     def reset(self) :
-        self.w = randint(self.width)
-        self.h = randint(self.height)
+        self.w = random.randint(0, self.width-1)
+        self.h = random.randint(0, self.height-1)
         self.isLoaded = False
-        self.map = np.zeroes((self.width, self.height))
+        self.map = np.zeros((self.width, self.height))
 
-        self.map[randint(self.width), randint(self.height)] += 5
-        self.map[randint(self.width), randint(self.height)] -= 5
+        self.map[random.randint(0, self.width-1), random.randint(0, self.height-1)] += 5
+        self.map[random.randint(0, self.width-1), random.randint(0, self.height-1)] -= 5
 
         return self._computeObservation()
 
 
     def _isMapFlat(self) :
         sum = np.sum(np.abs(self.map))
-        return sum == 0
+        return bool(sum == 0) # Cast <np._bool> to <bool>
 
 
     def step(self, action) :
 
         previousHeight = self.map[self.w, self.h]
+        previousW = self.w
+        previousH = self.h
 
         if (action == self.GoUp) :
             if (self.h < self.height-1) :
@@ -87,14 +90,12 @@ class ConstructionSite(gym.Env) :
             if (self.isLoaded) :
                 self.isLoaded = False
                 self.map[self.w, self.h] += 1
-        raise ValueError(f"Recieved invalid action '{action}'.")
+        else :
+            raise ValueError(f"Recieved invalid action '{action}'.")
 
+        obs = self._computeObservation()
+        reward = -0.01
+        reward += abs(previousHeight) - abs(self.map[previousW, previousH])
         done = self._isMapFlat()
-
-        reward = -0.05
-        if (abs(self.map[self.w, self.h]) < abs(previousHeight)) :
-            reward += 1
-
         info = {}
-
-        return self._computeObservation(), reward, done, info
+        return obs, reward, done, info
